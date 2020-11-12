0962b937686c8c714e4491b3e69731858b2b03f1
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2020-11-12 17:05:51 -0500

    Initial commit.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Store user's registration information
+        $validatedData = $request->validate([
+            'first_name'=>'required|max:55',
+            'last_name'=>'required|max:55',
+            'email'=>'email|required|unique:users',
+            'password'=>'required|confirmed'
+        ]);
+
+        // Encrypt password
+        $validatedData['password'] = bcrypt($request->password);
+
+        // Create user using User Model
+        $user = User::create($validatedData);
+
+        // Create token and assign it to the user
+        $accessToken = $user->createToken('authToken')->accessToken;
+        
+        return response(['user' => $user, 'access_token' => $accessToken]);
+    }
+
+    public function login(Request $request)
+    {
+        // Store user's credentials
+        $loginData = $request->validate([
+            'email'=>'email|required',
+            'password'=>'required'
+        ]);
+
+        // Attempt to authenticate user
+        if(!auth()->attempt($loginData)) {
+            return response(['message' => 'Invalid email or password.']);
+        }
+
+        // If user is registered, assign a token
+        $accessToken = auth()->user()->createToken('authToken')->accessToken;
+
+        return response(['user' => auth()->user(), 'access_token' => $accessToken]);
+    }
+} 
